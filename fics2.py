7b0ae76aa2103a01a615a6a07a3a12cb3e22bf9a
manfred <pipitos.ontherocks@gmail.com>
2016-07-17 17:26:16 +1000

    Second commit - this time it runs commands and display results

--- fics2.py
+++ fics2.py
@@ -3,6 +3,11 @@ import sys
 import telnetlib
 import time
 
+
+def right(s, amount):
+    return s[-amount:]
+
+
 def login(host, handle, password, verbose=False):
 
 	# Connect to FICS
@@ -23,11 +28,10 @@ def login(host, handle, password, verbose=False):
 		print "Logging in as " + handle + ", please wait..."
 	
 	if handle == "guest":
-#    		print f.read_all()
 		f.read_until(":\n\r")
 		f.write("\n\r")
 		if verbose:
-			print "Logged in as guest - no password needed"
+			print "Logging in as guest - no password needed"
 	else:
     		f.read_until("password: ")
 		try:
@@ -38,12 +42,10 @@ def login(host, handle, password, verbose=False):
 			print "Password accepted :)"
 
 	outp = f.read_until ("fics%", timeout=1)
-	print ("{" + outp + "}")
+#	print ("{" + right(outp, 5) + "}")
 
 
-	time.sleep(5)
-
-	if (outp == ' '):
+	if (right(outp, 5) <> 'fics%'):
 		if verbose:
 			print "No prompt received"
 	else:
@@ -53,11 +55,49 @@ def login(host, handle, password, verbose=False):
 	return f
 
 
+def fics_cmd (session, command, completion_OK_text):
+	try:
+		session.read_very_eager()
+		session.write(command + "\n\r")
+		
+		outp = session.read_until (completion_OK_text, timeout=1)
+		if right(outp, len(completion_OK_text)) == completion_OK_text:
+			return outp
+		else:
+			return ""
+
+	except:
+		raise Error("Unsuccessful command execution")
+
+
+
+
 host = "freechess.org"
 user = "guest"
 tn = login(host,user,"",True)
 print "Connected!"
 
-tn.write("games" + "\r\n")
-print tn.read_all()
-	
+
+if fics_cmd(tn, "set seek 0", "You will not see seek ads."):
+	print "No more seek ads!"
+else:
+	print "Turning seek ads off unsuccessful"
+
+
+# print fics_cmd(tn, "games", "games displayed.")
+
+print fics_cmd(tn, "finger drakem", "fics%")
+
+
+
+
+
+#tn.write("set seek 0" + "\n\r")
+#outp = tn.read_until ("fics%", timeout=1)
+#print ("{" + outp + "}")
+#print tn.read_all()
+
+#tn.write("games" + "\n\r")
+#outp = tn.read_until ("fics%", timeout=1)
+#print ("{" + outp + "}")
+#print tn.read_all()
